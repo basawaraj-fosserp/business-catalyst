89ddb2bb2d2351f99ce262951054040c68c72312
viralpatel15 <viralkansodiya167@gmail.com>
2024-02-12 11:10:14 +0000

    custom field and doctype

---
+++ business_catalyst/business_catalyst/doctype/location_name/location_name.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Viral Patel and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Location Name', {
+	// refresh: function(frm) {
+
+	// }
+});
