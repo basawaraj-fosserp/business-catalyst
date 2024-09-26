7d2ab4442a982cd0680cdfe36e7ada243cd21976
viralpatel15 <viralkansodiya167@gmail.com>
2024-09-26 09:27:26 +0000

    custom_field

--- business_catalyst/public/js/task.js
+++ business_catalyst/public/js/task.js
@@ -1,17 +1,17 @@
-frappe.ui.form.on("Task",{
-   refresh:function(frm){
-    console.log("Hello")
-   },
-   custom_update_questions:frm=>{
-        frappe.call({
-            method : "business_catalyst.business_catalyst.task.get_question",
-            args :{
-                "project_template" : frm.doc.custom_project_template 
-            },
-            callback:function(r){
-                console.log(r.message)
-            }
-        })
-   }
+// frappe.ui.form.on("Task",{
+//    refresh:function(frm){
+//     console.log("Hello")
+//    },
+//    custom_update_questions:frm=>{
+//         frappe.call({
+//             method : "business_catalyst.business_catalyst.task.get_question",
+//             args :{
+//                 "project_template" : frm.doc.custom_project_template 
+//             },
+//             callback:function(r){
+//                 console.log(r.message)
+//             }
+//         })
+//    }
    
-})
+// })
