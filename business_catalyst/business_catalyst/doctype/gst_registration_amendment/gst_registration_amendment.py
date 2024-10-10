58e628b0cbb469429fb01186fedede58af5cca2f
viralpatel15 <viralkansodiya167@gmail.com>
2024-10-10 11:31:11 +0000

    ch

--- business_catalyst/business_catalyst/doctype/gst_registration_amendment/gst_registration_amendment.py
+++ business_catalyst/business_catalyst/doctype/gst_registration_amendment/gst_registration_amendment.py
@@ -4,5 +4,5 @@
 # import frappe
 from frappe.model.document import Document
 
-class GSTRegisterationAmendment(Document):
+class GSTRegistrationAmendment(Document):
 	pass
--- business_catalyst/business_catalyst/doctype/gst_registration_amendment/test_gst_registration_amendment.py
+++ business_catalyst/business_catalyst/doctype/gst_registration_amendment/test_gst_registration_amendment.py
@@ -5,5 +5,5 @@
 from frappe.tests.utils import FrappeTestCase
 
 
-class TestGSTRegisterationAmendment(FrappeTestCase):
+class TestGSTRegistrationAmendment(FrappeTestCase):
 	pass
