3d3a2aa85adebeee06f019263d0d580757eb3400
viralpatel15 <viralkansodiya167@gmail.com>
2024-10-10 08:50:01 +0000

    ch

--- business_catalyst/public/js/quotation.js
+++ business_catalyst/public/js/quotation.js
@@ -1,4 +1,12 @@
 frappe.ui.form.on("Quotation",{
+    setup:function(frm){
+        if(frm.doc.quotation_to == "Lead" && !frm.doc.contact_email){
+            frappe.model.get_value("Lead", frm.doc.party_name, ["custom_primary_email_id", "custom_secondary_email_id"], r=>{
+                console.log(r)
+                frm.set_value("contact_email", r.custom_primary_email_id)
+            })
+        }~
+    },
     party_name:function(frm){
         if(frm.doc.quotation_to == "Lead" && !frm.doc.contact_email){
             frappe.model.get_value("Lead", frm.doc.party_name, ["custom_primary_email_id", "custom_secondary_email_id"], r=>{
