21001cd219901346431234aa4c594c59e899907d
root <viral@fosserp.com>
2024-10-29 08:32:44 +0000

    new webforms

--- business_catalyst/business_catalyst/doctype/central_fssai/central_fssai.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) 2024, Viral Patel and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-class CentralFSSAI(Document):
-	pass
--- business_catalyst/business_catalyst/doctype/central_fssai/test_central_fssai.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2024, Viral Patel and Contributors
-# See license.txt
-
-# import frappe
-from frappe.tests.utils import FrappeTestCase
-
-
-class TestCentralFSSAI(FrappeTestCase):
-	pass
