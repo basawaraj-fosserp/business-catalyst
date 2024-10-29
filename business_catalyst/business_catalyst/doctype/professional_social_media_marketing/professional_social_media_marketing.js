d82613e9cfb6e2d8285e48c97714bbca55fa9878
root <viral@fosserp.com>
2024-10-29 08:39:45 +0000

    new doctype

---
+++ business_catalyst/business_catalyst/doctype/professional_social_media_marketing/professional_social_media_marketing.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Viral Patel and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Professional Social Media Marketing', {
+	// refresh: function(frm) {
+
+	// }
+});
