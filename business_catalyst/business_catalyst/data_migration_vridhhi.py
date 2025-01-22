4578d0bf03c92f65757ec4c1c38c3b104338a067
viral <viral@fosserp.com>
2025-01-22 05:41:05 +0000

    ch

--- business_catalyst/business_catalyst/data_migration_vridhhi.py
+++ business_catalyst/business_catalyst/data_migration_vridhhi.py
@@ -132,11 +132,11 @@ def migrate_in_json():
                     })
                 if (d.get("ERP Column") == "mobile_no") and row.get(d.get("Dwani Column")):
                     lead.update({
-                            'mobile_no' : str(row.get(d.get("Dwani Column")))
+                            'mobile_no' : str(row.get(d.get("Dwani Column"))).replace("-","").replace(" ","").replace(" ","")
                         })  
                 if (d.get("ERP Column") == "phone") and row.get(d.get("Dwani Column")):
                     lead.update({
-                            'phone' : str(row.get(d.get("Dwani Column")))
+                            'phone' : str(row.get(d.get("Dwani Column"))).replace("-","").replace(" ","").replace(" ","").split(",")[0]
                         })
                 if (d.get("ERP Column") == "custom_business_type1"):
                     if row.get(d.get("Dwani Column")) == "Trader":
