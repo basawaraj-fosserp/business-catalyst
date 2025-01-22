43671af3de14cd15c12d784ec1c5d4f3c01fcd30
viral <viral@fosserp.com>
2025-01-22 13:09:46 +0000

    ch

--- business_catalyst/business_catalyst/data_migrate_sheet.py
+++ business_catalyst/business_catalyst/data_migrate_sheet.py
@@ -156,7 +156,8 @@ def migrate_2_in_json():
                 continue
             lead = validate_address(lead)
             if not lead.get("first_name") and not lead.get("company_name") and not lead.get("email_id"):
-                continue
+                lead.update({"first_name" : "unknown"})
+                lead.update({"company_name" : "unknown"})
             doc = frappe.get_doc(lead)
             print(row.get("id"))
             doc.insert(ignore_mandatory=True)
