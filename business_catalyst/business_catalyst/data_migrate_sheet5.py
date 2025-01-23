4332721dc220807c237bb17a9be0b09b69bf7fbd
viral <viral@fosserp.com>
2025-01-23 05:16:49 +0000

    ch

--- business_catalyst/business_catalyst/data_migrate_sheet5.py
+++ business_catalyst/business_catalyst/data_migrate_sheet5.py
@@ -87,7 +87,7 @@ columns_mapping = [
         }
         
     ]
-def migrate_2_in_json():
+def migrate_sheet5_in_json():
     filename = "output_file_part_5.xlsx"
     init_path = "/home/frappe/frappe-bench/sites"+get_file_path(filename)[1:]
 
