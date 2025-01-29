292b9e12f952b5100164f9255f8655b014fb4626
viral <viral@fosserp.com>
2025-01-29 14:27:45 +0000

    ch

--- business_catalyst/business_catalyst/data_migration_vridhhi.py
+++ business_catalyst/business_catalyst/data_migration_vridhhi.py
@@ -18,7 +18,7 @@ def migrate_in_json():
     json_data = json.loads(json_data)
     fail_lead = []
     count = 0
-    for row in json_data[45930:]:
+    for row in json_data[46228:]:
         if not frappe.db.exists("Lead", {"custom_dwani_erp_id" : row.get("id")}):
             error = stop_duplicate_lead(row)
             if error:
