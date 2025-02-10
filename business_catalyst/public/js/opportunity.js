5b56e44e4ee05521d2e83c5be549e1b70da49b79
viral <viral@fosserp.com>
2025-02-10 03:57:19 +0000

    remove quotation button if status is not open

--- business_catalyst/public/js/opportunity.js
+++ business_catalyst/public/js/opportunity.js
@@ -1,5 +1,10 @@
 frappe.ui.form.on('Opportunity', {
     onload:frm=>{
         cur_frm.$wrapper.find(".form-shared").addClass("hidden")
+    },
+    refresh:frm=>{
+        if(frm.doc.status != "Open"){
+            frm.remove_custom_button("Quotation", "Create")
+        }
     }
 })
