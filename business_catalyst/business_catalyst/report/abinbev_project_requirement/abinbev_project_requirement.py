f80c285d4ef4a0d1bf711ff205353994196e5fb4
viral <viral@fosserp.com>
2025-02-12 12:06:36 +0000

    ch

--- business_catalyst/business_catalyst/report/abinbev_project_requirement/abinbev_project_requirement.py
+++ business_catalyst/business_catalyst/report/abinbev_project_requirement/abinbev_project_requirement.py
@@ -30,7 +30,7 @@ def get_opportunity_data(filters = None):
 					  opp.custom_business_category,
 					  opp.custom_business_type1,
 					  opp.custom_annual_turnover,
-					  opp.custom_tagged_advisor,
+					  opp.custom_tagged_advisor  as custom_tagged_se_salesperson,
 					  opp_item.item_code
 					  From `tabOpportunity` as opp
 					  Left Join `tabOpportunity Item` as opp_item ON opp_item.parent = opp.name
