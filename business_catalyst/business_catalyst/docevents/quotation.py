2e635a92a883454137ae2c29c28fceda13478aaa
viral <viral@fosserp.com>
2025-03-29 02:40:20 +0000

    ch

--- business_catalyst/business_catalyst/docevents/quotation.py
+++ business_catalyst/business_catalyst/docevents/quotation.py
@@ -40,13 +40,13 @@ def calculate_payment_amount(self):
             total_amount_item = row.base_net_amount * 0.18 + row.base_net_amount
         if self.total_taxes_and_charges == 0:
             total_amount_item = row.base_net_amount
-        outstanding_amount_item = row.total_amount - row.paid_amount
+        outstanding_amount_item = total_amount_item - row.paid_amount
         frappe.db.sql(f"""
                     Update `tabQuotation Item`
                     Set total_amount = '{total_amount_item}', outstanding_amount = '{outstanding_amount_item}'
                     Where name = '{row.name}'
                 """, as_dict=1)
-        outstanding_amount += row.outstanding_amount
+        outstanding_amount += outstanding_amount_item
         allocated_amount += row.paid_amount
     self.outstanding_amount = outstanding_amount
     self.allocated_amount = allocated_amount
